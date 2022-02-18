91a2a2c5ad745e44d42a47e93ffd23cbdc172193
qiangaaaaa <2550358316@qq.com>
2022-02-18 20:24:12 +0800

    调整评论、回复排版

--- src/components/article/CommentGroup.vue
+++ src/components/article/CommentGroup.vue
@@ -2,27 +2,41 @@
   <div class="commentGroup">
     <!--评论-->
     <div class="comment">
-      <span>{{ comment.commentNickname }}</span>
-      <span>{{ comment.commentTime }}</span>
+      <p>
+        <span>{{ comment.commentNickname }}</span>
+      </p>
       <p class="content">{{ comment.content }}</p>
+      <p>
+        <span class="timespan">{{ comment.commentTime }}</span>
+        <span class="pointer replyspan">回复</span>
+      </p>
     </div>
     <!--回复-->
     <div class="reply">
-      <div class="replyBody" v-for="reply in comment.replyResponseVOS.slice(0,2)">
-        <span>{{ reply.replyNickname }}</span>
-        <span v-if="reply.parentReplyId != 0">
-          回复 {{ reply.targetNickName }}：</span
-        >
-        <span>{{ reply.content }}</span>
+      <div
+        class="replyBody"
+        v-for="reply in comment.replyResponseVOS.slice(0, 2)"
+      >
+        <p class="content">
+          <span>{{ reply.replyNickname }}</span>
+          <span v-if="reply.parentReplyId != 0">
+            回复 {{ reply.targetNickName }}：
+          </span>
+          <span>{{ reply.content }}</span>
+        </p>
         <p>
           <span class="timespan">{{ reply.replyTime }}</span>
           <span class="pointer replyspan">回复</span>
         </p>
       </div>
-      <p class="replystatistics" v-if="comment.replyResponseVOS.length > 2"><span>共 {{comment.replyResponseVOS.length}} 条回复</span> <span class="pointer">查看全部</span> </p>
-      <div class="replyadd">
+      <p class="replystatistics" v-if="comment.replyResponseVOS.length > 2">
+        <span>共 {{ comment.replyResponseVOS.length }} 条回复</span>
+        <span class="pointer">查看全部</span>
+      </p>
 
-      </div>
+      <!-- 添加回复组件 -->
+      <!-- <reply-add /> -->
+      <div class="replyadd"></div>
     </div>
   </div>
 </template>
@@ -46,12 +60,15 @@ export default {
 </script>
 
 <style>
-
 .commentGroup::after {
   content: "";
   display: block;
   height: 0;
   clear: both;
+}
+
+.content {
+  margin-bottom: 10px;
 }
 
 /* 回复相关的css */
@@ -61,11 +78,15 @@ export default {
 }
 
 .replyBody {
-  margin-bottom: 12px;
+  margin-bottom: 20px;
+}
+
+.replyspan {
+  margin-left: 20px;
 }
 
 .replyspan:hover {
-  color: #409EFF
+  color: #409eff;
 }
 
 /* 指针样式 */
